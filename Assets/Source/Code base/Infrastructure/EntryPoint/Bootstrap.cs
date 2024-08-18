1d644783aaba7164971aec5e8ca6157a2d871196
stanislavno <stanislav_developer@mail.ru>
2024-08-18 06:53:10 +0500

    +

---
+++ Assets/Source/Code base/Infrastructure/EntryPoint/Bootstrap.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Source.Code_base
+{
+    public class Bootstrap : MonoBehaviour, ICoroutineRunner
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
