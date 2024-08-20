ff5093dcc122732c0c03c74d502b041eb79639c5
stanislavno <stanislav_developer@mail.ru>
2024-08-20 13:44:15 +0500

    add weapon logic

--- Assets/Source/Code base/Entyties/Character/Character.cs
+++ Assets/Source/Code base/Entyties/Character/Character.cs
@@ -6,16 +6,17 @@ public class Character : MonoBehaviour, IReadOnlyCharacter
 {
     private Rotator _rotator;
     private Mover _mover;
+    private Weapon _weapon;
     private IInputService _input;
 
     public CharacterStats Stats { get; private set; }
 
-    public void Init(IInputService input)
+    public void Init(IInputService input, IWeaponStat weaponStat)
     {
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
 
         _input = input;
-        Stats = new(rigidbody, transform, new Weapon());
+        Stats = new(rigidbody, transform, new Weapon(_input, weaponStat));
 
         _rotator = new(input, transform);
         _mover = new(input, rigidbody, transform);
---
+++ Assets/Source/Code base/Entyties/Weapon/BulletPool.cs
@@ -0,0 +1,13 @@
+﻿namespace Assets.Source.Code_base
+{
+    public class BulletPool
+    {
+        private readonly IWeaponStat _weaponStat;
+
+        public BulletPool(IWeaponStat weaponStat)
+        {
+            _weaponStat = weaponStat;
+        }
+
+    }
+}
