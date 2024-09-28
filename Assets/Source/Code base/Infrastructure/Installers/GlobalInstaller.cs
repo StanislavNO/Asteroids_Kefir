05f31a8859f00ddaf9abbd6a50caa1b313795850
Stanislav <130127229+StanislavNO@users.noreply.github.com>
2024-09-28 13:07:59 +0500

    Add Zenject

---
+++ Assets/Source/Code base/Infrastructure/Installers/GlobalInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Zenject;
+
+
+namespace Assets.Source.Code_base
+{
+    public class GlobalInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            BindInput();
+        }
+
+        private void BindInput()
+        {
+            Container.BindInterfacesTo<StandaloneInput>().AsSingle();
+        }
+    }
+}
