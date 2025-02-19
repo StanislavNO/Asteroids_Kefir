f54b98451d0bb7f0ec67d32c896dff55fc7dde79
Stanislav <130127229+StanislavNO@users.noreply.github.com>
2025-02-19 14:48:36 +0500

    +

--- Assets/_Source/CodeBase/Meta/Infrastructure/Installers/MetaGlobalInstaller.cs
+++ Assets/_Source/CodeBase/Meta/Infrastructure/Installers/MetaGlobalInstaller.cs
@@ -1,4 +1,6 @@
-﻿using Assets._Source.CodeBase.Meta.Services.InApp;
+﻿using Assets._Source.CodeBase.Meta.Services.Analytics;
+using Assets._Source.CodeBase.Meta.Services.Analytics.SoftDevKits;
+using Assets._Source.CodeBase.Meta.Services.InApp;
 using Zenject;
 
 namespace Assets._Source.CodeBase.Meta.Infrastructure.Installers
@@ -8,6 +10,24 @@ namespace Assets._Source.CodeBase.Meta.Infrastructure.Installers
         public override void InstallBindings()
         {
             BindInAppService();
+            BindAnalytics();
+        }
+
+        private void BindAnalytics()
+        {
+            Container
+                .Bind<IAnalyticProvider>()
+                .To<FirebaseAnalytic>()
+                .AsSingle();
+
+            Container
+                .BindInterfacesAndSelfTo<EventCounter>()
+                .AsSingle();
+
+            Container
+                .BindInterfacesTo<AnalyticsController>()
+                .AsSingle()
+                .NonLazy();
         }
 
         private void BindInAppService()
