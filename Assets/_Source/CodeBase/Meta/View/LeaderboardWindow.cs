e77d6af54fd695282951f4ab02e93aa2c7f2a7f4
Stanislav <130127229+StanislavNO@users.noreply.github.com>
2025-03-02 16:00:11 +0500

    +

--- Assets/_Source/CodeBase/Meta/View/LeaderboardWindow.cs
+++ Assets/_Source/CodeBase/Meta/View/LeaderboardWindow.cs
@@ -5,19 +5,19 @@ namespace Assets._Source.CodeBase.Meta.View
 {
     public class LeaderboardWindow : MonoBehaviour
     {
+        [SerializeField] private LeaderView _leaderPrefab;
+        [SerializeField] private Transform _leaderboardContainer;
+             
         [field: SerializeField] public int MaxLeaders { get; private set; }
-        
-        public LeaderView leaderPrefab;
-        public Transform leaderboardContainer;
 
         public void Show(List<int> scores)
         {
-            foreach (Transform child in leaderboardContainer)
+            foreach (Transform child in _leaderboardContainer)
                 Destroy(child.gameObject);
             
             for (int i = 0; i < scores.Count; i++)
             {
-                LeaderView entry = Instantiate(leaderPrefab, leaderboardContainer);
+                LeaderView entry = Instantiate(_leaderPrefab, _leaderboardContainer);
                 entry.Score.text = $"{i + 1}. = {scores[i]} очков";
             }
         }
